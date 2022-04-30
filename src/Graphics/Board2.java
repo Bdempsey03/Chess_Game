2e76575c773e610c8564af71bca6ae1ca7268294
Ben Dempsey <bdempsey@unbc.ca>
2022-04-29 21:18:47 -0700

    added a couple more pieces

--- src/Graphics/Board2.java
+++ src/Graphics/Board2.java
@@ -1,6 +1,6 @@
 package Graphics;
 
-import Pieces.RookModel;
+import Pieces.*;
 
 import static Graphics.BoardSquare.theBoard;
 
@@ -8,5 +8,10 @@ public class Board2{
 
     public static void boardSetup(){
         theBoard[0][0].setPieceOnSquare(new RookModel('w'));
+        theBoard[7][0].setPieceOnSquare(new RookModel('w'));
+        theBoard[1][0].setPieceOnSquare(new KnightModel('w'));
+        theBoard[6][0].setPieceOnSquare(new KnightModel('w'));
+        theBoard[2][0].setPieceOnSquare(new BishopModel('w'));
+        theBoard[5][0].setPieceOnSquare(new BishopModel('w'));
     }
 }
